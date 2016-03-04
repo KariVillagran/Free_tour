a55ebd786e4d81f5b1baa16c5759f57387422ba1
Ana Villagran <akarinavillagrani@gmail.com>
2016-03-03 21:06:49 -0300

    publicacion
    version publicada 2014

---
+++ js/English/envia.php
@@ -0,0 +1,26 @@
+﻿<?php
+$remitente = $_POST['correo_input'];
+$destinatario = 'chilefreetour@gmail.com'; 
+$asunto = 'Consulta';
+if (!$_POST){
+?>
+
+<?php
+}else{
+	 
+    $cuerpo = "Nombre: " . $_POST["nombre_input"] . "\r \n"; 
+    $cuerpo .= "Email: " . $_POST["correo_input"] . "\r \n";
+	$cuerpo .= "Consulta: " . $_POST["mensaje_input"] . "\r\n";
+
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST['nombre_input']."\" <".$remitente.">\n";
+
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include 'confirma.html'; //se debe crear un html que confirma el envío
+}
+?>
